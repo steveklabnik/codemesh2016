a3e23db5046eb69ca9aaa6c011c50d9347b447ff
Steve Klabnik <steve@steveklabnik.com>
2016-11-04 06:21:23 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,11 @@
+# Exploring Ruby through Rust: CodeMesh 2016
+
+My talk from CodeMesh 2016.
+
+## up and running
+
+you can optionally use a Node.js workflow for auto-reload:
+
+1. `npm install`
+2. `npm start`
+3. Visit [`http://localhost:8080`]
---
+++ mem.rb
@@ -0,0 +1,31 @@
+require 'objspace'
+require 'pp'
+
+print "OBJECT SIZE: "
+
+size = ObjectSpace.memsize_of(Object::new)
+pp size
+
+def allocate_count
+  GC.disable
+  before = ObjectSpace.count_objects
+  yield
+  after = ObjectSpace.count_objects
+  after.each { |k,v| after[k] = v - before[k] }
+  GC.enable
+  after
+end
+
+counts = allocate_count {
+  /\A[[:space:]]*\z/ === "Привет";
+}
+
+counts.delete(:FREE)
+counts.delete(:TOTAL)
+counts.delete_if {|_,v| v == 0}
+
+print "COUNTS: "
+pp counts
+
+print "MATH: "
+pp (counts.values.inject(0, :+) * size)
